8731848482bd0f7d439e2dc899d51ae15f22520a
ogu <oguzkoroglu@gmail.com>
2017-05-26 23:24:33 +0200

    Package Refactoring

---
+++ app/src/main/java/com/watchmen/cloudwallet/Models/WalletVM.java
@@ -0,0 +1,55 @@
+package com.watchmen.cloudwallet.Models;
+
+import org.simpleframework.xml.Attribute;
+import org.simpleframework.xml.ElementList;
+import org.simpleframework.xml.Root;
+import org.simpleframework.xml.Serializer;
+import org.simpleframework.xml.core.Persister;
+
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+
+@Root
+public class WalletVM extends VMBase {
+    @ElementList
+    public ArrayList<ItemVM> Items;
+    @Attribute
+    public String FileName;
+    @Attribute
+    public String Password;
+
+    public Collection<ItemVM> FilteredItems(String key) {
+
+        ArrayList<ItemVM> filtered = new ArrayList<>();
+        for (ItemVM item : Items)
+            if (item.Title.contains(key))
+                filtered.add(item);
+
+        Collections.sort(filtered, new Comparator<ItemVM>() {
+            @Override
+            public int compare(ItemVM o1, ItemVM o2) {
+                return o1.Title.compareToIgnoreCase(o2.Title);
+            }
+        });
+
+        return filtered;
+    }
+
+    public static WalletVM DeSerializeFromXML(String xml) throws Exception {
+        Serializer serializer = new Persister();
+        WalletVM result = serializer.read(WalletVM.class, xml);
+        return result;
+    }
+
+    public String SerializeToXML(String fileName) throws Exception {
+        Serializer serializer = new Persister();
+        StringWriter wr = new StringWriter();
+        serializer.write(this, wr);
+        wr.close();
+        String result = wr.toString();
+        return result;
+    }
+}
